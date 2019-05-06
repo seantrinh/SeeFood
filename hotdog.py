6de5aedad38e54700760468dd79b0d516569c801
Sean Trinh <strinh@stevens.edu>
2019-05-06 00:33:10 -0400

    initial model. 56% accuracy

---
+++ hotdog.py
@@ -0,0 +1,90 @@
+import numpy as np
+import os
+from keras.preprocessing.image import load_img, img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.resnet50 import preprocess_input
+from keras import Sequential
+from keras.layers import Flatten, Dense, Conv2D, Dropout, BatchNormalization, Activation
+from keras import optimizers
+
+train_path = './data/train'
+test_path = './data/test'
+hot_dog_path = './data/train/hot_dog'
+not_hot_dog_path = './data/train/not_hot_dog'
+
+train_data_hd = [os.path.join(hot_dog_path, filename)\
+                 for filename in os.listdir(hot_dog_path)]
+train_data_nhd = [os.path.join(not_hot_dog_path, filename)\
+                  for filename in os.listdir(not_hot_dog_path)]
+
+img_size = 224
+num_classes = 2 # hot dog or not hot dog
+
+data_generator = ImageDataGenerator()
+
+train_generator = data_generator.flow_from_directory(
+    train_path,
+    target_size=(img_size, img_size),
+    batch_size=498,
+    class_mode='categorical')
+
+validation_generator = data_generator.flow_from_directory(
+    test_path,
+    target_size=(img_size, img_size),
+    batch_size=500,
+    class_mode='categorical')
+
+
+def read_and_prep_images(img_paths, img_height=img_size, img_width=img_size):
+    imgs = [load_img(img_path, target_size=(img_height, img_width)) for img_path in img_paths]
+    img_array = np.array([img_to_array(img) for img in imgs])
+    output = preprocess_input(img_array)
+    return output
+
+
+train_data = read_and_prep_images(train_data_nhd)
+train_data1 = read_and_prep_images(train_data_hd)
+
+model = Sequential()
+model.add(Conv2D(8, kernel_size=(3,3), input_shape=(img_size, img_size, 3)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(8, kernel_size=(3,3)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(8, kernel_size=(3,3)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(8, kernel_size=(3,3)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Conv2D(8, kernel_size=(3,3)))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(BatchNormalization())
+model.add(Activation('relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+#LEARNING RATE
+learning_rate = 1E-3
+model.compile(loss='categorical_crossentropy',
+              optimizer=optimizers.RMSprop(lr=learning_rate),
+              metrics=['acc'])
+
+input_x = (train_generator[0][0]/255)
+input_y = (train_generator[0][1])
+
+model.fit(input_x,
+          input_y,
+          batch_size=24,
+          epochs=5)
+
+output_x = (validation_generator[0][0]/255)
+output_y = validation_generator[0][1]
+
+loss_and_acc = model.evaluate(output_x, output_y)
+print('loss = ' + str(loss_and_acc[0]))
+print('accuracy = ' + str(loss_and_acc[1]))
